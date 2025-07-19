f5ba9fafdc9f34219f3f10cade042f8e6dabdb74
VadlamudiSreenadh2003 <vadlamudisreenadh@gmail.com>
2025-07-19 10:48:38 +0530

    Update BookingRepository.java

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -1,21 +1,4 @@
 package com.example.demo.repository;
-// 
-////import org.springframework.data.jpa.repository.JpaRepository;
-////
-////import com.example.demo.Entity.Booking;
-////
-////public interface BookingRepository extends JpaRepository<Booking, Long> {
-////}
-// 
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import com.example.demo.Entity.Booking;
-
-//public interface BookingRepository extends JpaRepository<Booking, Long> {
-//    // Custom queries can be added here if needed
-//}
- 
-
- 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
